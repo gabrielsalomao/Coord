59fb5a7eaa103dc7a6396a43a07e1ccee610674a
sajgabriel <sajgabrielsalomao@gmail.com>
2020-05-25 08:11:00 -0400

    first commit

---
+++ Coord/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Coord.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
